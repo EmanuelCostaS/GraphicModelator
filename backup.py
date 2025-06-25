ce759e84618c52a7cd854e61777ffd81de148d2d
Gabriel Marques Arantes <2gma26062004@gmail.com>
2025-06-24 23:41:25 -0300

    correção backup e teste
    ainda a curva não funciona, corrigi as cores

--- backup.py
+++ backup.py
@@ -149,7 +149,7 @@ def hexagon(radius, height, position, color, scale_factor=1.0, squash_factor=0.8
     if draw_lines:
         glDisable(GL_LIGHTING)
         line_indices = [0, 2, 4]
-        z_line = height / 2.0
+        z_line = height / 2.0 + 0.05 # pra ficar um pouco acima do topo do hexágono
         glColor3f(*line_color)
         glPointSize(line_width)
         rasterization_scale = 100.0
@@ -202,28 +202,28 @@ def draw_scene(camera, is_perspective, width, height):
     curve_angle = 60 # Um ângulo maior pode dar um resultado melhor
 
     # Hexágono 1
-    hexagon(1.5, 2, [0, 0, 0], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(1.5, 2, [0, 0, 0], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 2
-    hexagon(2.7, 2, [0, 0, -2], (0.004, 0.404, 0.62), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(2.7, 2, [0, 0, -2], (0.004, 0.404, 0.62), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 3
-    hexagon(3.9, 2, [0, 0, -4], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(3.9, 2, [0, 0, -4], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 4
-    hexagon(6.9, 2, [0, 0, -6], (0.553, 0.557, 0.573), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(6.9, 2, [0, 0, -6], (0.553, 0.557, 0.573), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 5
-    hexagon(8.1, 2, [0, 0, -8], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(8.1, 2, [0, 0, -8], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 6
-    hexagon(15.8, 2, [0, 0, -10], (0.004, 0.404, 0.62), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(15.8, 2, [0, 0, -10], (0.004, 0.404, 0.62), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 7
-    hexagon(17, 2, [0, 0, -12], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(17, 2, [0, 0, -12], (1, 1, 1), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
     
     # Hexágono 8
-    hexagon(17.6, 2, [0, 0, -14], (0.553, 0.557, 0.573), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(0.3,0.3,0.3), line_width=20.0)
+    hexagon(17.6, 2, [0, 0, -14], (0.2, 0.2, 0.2), scale_factor=scale_factor, squash_factor=squash, curve_angle_deg=curve_angle, line_color=(1,1,1), line_width=20.0)
 
     pygame.display.flip()
 
